3f8bfad41815a7f7ca1a9b2fa6cdbbd933c4e564
Soraya Vay <vay1982@gmail.com>
2013-02-19 00:06:20 +0100

    Funcionalidad Boton @

--- gen/es/sorayavay/listado/R.java
+++ gen/es/sorayavay/listado/R.java
@@ -42,27 +42,31 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_galeria=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_menu=0x7f030002;
-        public static final int item_categoria=0x7f030003;
-        public static final int segunda_activity=0x7f030004;
-        public static final int tercera_main=0x7f030005;
+        public static final int activity_info=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_menu=0x7f030003;
+        public static final int item_categoria=0x7f030004;
+        public static final int segunda_activity=0x7f030005;
+        public static final int tercera_main=0x7f030006;
     }
     public static final class menu {
         public static final int activity_galeria=0x7f070000;
-        public static final int activity_main=0x7f070001;
-        public static final int activity_menu=0x7f070002;
-        public static final int tercera_main=0x7f070003;
+        public static final int activity_info=0x7f070001;
+        public static final int activity_main=0x7f070002;
+        public static final int activity_menu=0x7f070003;
+        public static final int tercera_main=0x7f070004;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
         public static final int boton_irmain=0x7f050007;
         public static final int galeria=0x7f050001;
+        public static final int hello_world=0x7f05000b;
         public static final int menu=0x7f050008;
         public static final int menu_settings=0x7f050002;
         public static final int texto_boton=0x7f050004;
         public static final int texto_hint=0x7f050005;
         public static final int title_activity_galeria=0x7f05000a;
+        public static final int title_activity_info=0x7f05000c;
         public static final int title_activity_menu=0x7f050009;
         public static final int title_activity_tercera=0x7f050006;
         public static final int titulo_listado=0x7f050003;
--- src/es/sorayavay/listado/MenuActivity.java
+++ src/es/sorayavay/listado/MenuActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageButton;
 public class MenuActivity extends Activity implements OnClickListener  {
 
 	ImageButton btnGaleria;
+	ImageButton btnContacta;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,9 @@ public class MenuActivity extends Activity implements OnClickListener  {
 		
 		btnGaleria = (ImageButton) findViewById(R.id.botonGaleria);
 		btnGaleria.setOnClickListener(this);
+		
+		btnContacta = (ImageButton) findViewById(R.id.botonContacta);
+		btnContacta.setOnClickListener(this);
 	}
 
 	@Override
@@ -31,15 +35,23 @@ public class MenuActivity extends Activity implements OnClickListener  {
 	@Override
 	public void onClick(View arg0) {
 		Intent intent = new Intent(this,GaleriaActivity.class);
+		Intent intentContacta = new Intent(Intent.ACTION_SEND);
 		switch (arg0.getId()) {
 		case R.id.botonGaleria:
 			startActivity(intent);
 			break;
-
+		case R.id.botonContacta:			
+			intentContacta.setType("plain/text");
+			intentContacta.putExtra(Intent.EXTRA_EMAIL, new String[] { "soraya.vay@luceit.es" });
+			intentContacta.putExtra(Intent.EXTRA_SUBJECT, "Galería de imágenes");
+			intentContacta.putExtra(Intent.EXTRA_TEXT, "Hola, mensaje enviado desde la aplicación de Galeria");
+			startActivity(Intent.createChooser(intentContacta, ""));
+			break;
 		default:
 			break;
 		}
 		
 	}
+
 
 }
