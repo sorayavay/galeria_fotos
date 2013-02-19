08153f891bad864616fd17a1b52d0f510f306ec6
Javier Gamarra <nhpatt@gmail.com>
2013-02-19 10:13:53 +0100

    Constructor, formateo

--- src/es/sorayavay/listado/DatoCategoria.java
+++ src/es/sorayavay/listado/DatoCategoria.java
@@ -2,28 +2,32 @@ package es.sorayavay.listado;
 
 import java.io.Serializable;
 
-public class DatoCategoria implements Serializable{
+public class DatoCategoria implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private String nombre;
 	private String descripcion;
+
+	public DatoCategoria(final String nombre, final String descripcion) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
-	public void setNombre(String nombre) {
+
+	public void setNombre(final String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
-	public void setDescripcion(String descripcion) {
+
+	public void setDescripcion(final String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
-
 
 }
